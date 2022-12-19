cc9992f44e167bf0d26f7fee2b98bf6dfad4d2de
leonhardsen4 <leonhardsen4@gmail.com>
2022-12-18 22:32:56 -0300

    18/12/2022 - Ajuste da tela "Ações de Entidade"; criação das tabelas "Endereços", "Unidades" e "Servidores".

--- src/main/java/br/gov/sp/procon/utils/Database.java
+++ src/main/java/br/gov/sp/procon/utils/Database.java
@@ -27,6 +27,41 @@ public class Database {
                       NOME TEXT    UNIQUE ON CONFLICT ROLLBACK
                                    NOT NULL
                   );
+                  
+                  --TABELA ENDEREÇOS--
+                  CREATE TABLE IF NOT EXISTS ENDERECOS (
+                      ID          INTEGER PRIMARY KEY AUTOINCREMENT,
+                      ID_ENTIDADE  INTEGER REFERENCES ENTIDADES (ID)
+                                          NOT NULL ON CONFLICT ROLLBACK,
+                      CEP         TEXT    NOT NULL ON CONFLICT ROLLBACK,
+                      LOGRADOURO  TEXT    NOT NULL ON CONFLICT ROLLBACK,
+                      NUMERO      TEXT,
+                      COMPLEMENTO TEXT,
+                      BAIRRO      TEXT,
+                      MUNICIPIO   TEXT    NOT NULL ON CONFLICT ROLLBACK,
+                      UF          TEXT    NOT NULL ON CONFLICT ROLLBACK
+                  );
+                  
+                  --TABELA UNIDADES__
+                  CREATE TABLE IF NOT EXISTS UNIDADES (
+                      ID                   INTEGER PRIMARY KEY AUTOINCREMENT,
+                      ID_ENTIDADE          INTEGER REFERENCES ENTIDADES (ID)
+                                                   NOT NULL ON CONFLICT ROLLBACK,
+                      ID_ENDERECO_ENTIDADE INTEGER REFERENCES ENDERECOS (ID)
+                                                   NOT NULL ON CONFLICT ROLLBACK,
+                      UNIDADE              TEXT    NOT NULL ON CONFLICT ROLLBACK
+                  );
+                  
+                  --TABELA SERVIDORES--
+                  CREATE TABLE IF NOT EXISTS SERVIDORES (
+                      ID         INTEGER PRIMARY KEY AUTOINCREMENT,
+                      NOME       TEXT    NOT NULL ON CONFLICT ROLLBACK,
+                      TELEFONE   TEXT,
+                      EMAIL      TEXT,
+                      ID_UNIDADE INTEGER REFERENCES UNIDADES (ID)
+                                         NOT NULL ON CONFLICT ROLLBACK,
+                      CARGO      TEXT
+                  );
                                                                
                 """);
         stmt.close();
